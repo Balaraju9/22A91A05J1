ce2361352921a6e3df8bc21da0d664d74bb1a93b
balaraju <119840117+Balaraju9@users.noreply.github.com>
2025-06-26 11:35:59 +0530

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,5 @@
 function logAction() {
   const logMessage = "Button clicked at " + new Date().toLocaleTimeString();
-  logToServer("frontend", "info", logMessage);  // use only lowercase values
+  logToServer("frontend", "info", logMessage); 
   document.getElementById("logOutput").innerText = logMessage;
 }
